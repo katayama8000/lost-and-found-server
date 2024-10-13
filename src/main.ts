b4d1a283c90b9ea3771eccf5206c5cab1e4af38e
katayama800 <tattu.0310@gmail.com>
2024-10-13 13:39:21 +0000

    Add push notification feature

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,26 @@
+import { Expo } from "expo-server-sdk";
+
+const expo = new Expo();
+
 Deno.cron("Push notification", "* * * * *", async () => {
-    console.log("This will push a message to your device every minute.");
-    const pushMessage = {
-        to: "ExponentPushToken[yueW4qNgnfYmJ8r7eiO0rA]",
+    const pushToken = "ExponentPushToken[yueW4qNgnfYmJ8r7eiO0rA]";
+
+    if (!Expo.isExpoPushToken(pushToken)) {
+        return;
+    }
+
+    const messages = [{
+        to: pushToken,
         title: "hello",
         body: "world",
-    };
+    }];
 
     try {
-        const response = await fetch("https://exp.host/--/api/v2/push/send", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(pushMessage),
-        });
-
-        if (!response.ok) {
-            console.error(
-                "Failed to send push notification:",
-                response.statusText,
-            );
-        } else {
-            console.log("Push notification sent successfully!");
+        const chunks = expo.chunkPushNotifications(messages);
+        for (const chunk of chunks) {
+            await expo.sendPushNotificationsAsync(chunk);
         }
     } catch (error) {
-        console.error("Error sending push notification:", error);
+        console.error(error);
     }
 });
