bed5ba164e8f1684e8e58fc637a2ef9a1e220ef4
katayama800 <tattu.0310@gmail.com>
2024-10-13 14:03:18 +0000

    remove to stop cron

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
-import { Expo } from "expo-server-sdk";
+// import { Expo } from "expo-server-sdk";
 
-const expo = new Expo();
+// const expo = new Expo();
+// Deno.cron("Push notification", "* * * * *", async () => {
+//     const pushToken = "ExponentPushToken[yueW4qNgnfYmJ8r7eiO0rA]";
 
-Deno.cron("Push notification", "* * * * *", async () => {
-    const pushToken = "ExponentPushToken[yueW4qNgnfYmJ8r7eiO0rA]";
+//     if (!Expo.isExpoPushToken(pushToken)) {
+//         return;
+//     }
 
-    if (!Expo.isExpoPushToken(pushToken)) {
-        return;
-    }
+//     const messages = [{
+//         to: pushToken,
+//         title: "hello",
+//         body: "world",
+//     }];
 
-    const messages = [{
-        to: pushToken,
-        title: "hello",
-        body: "world",
-    }];
+//     try {
+//         const chunks = expo.chunkPushNotifications(messages);
+//         for (const chunk of chunks) {
+//             await expo.sendPushNotificationsAsync(chunk);
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
 
-    try {
-        const chunks = expo.chunkPushNotifications(messages);
-        for (const chunk of chunks) {
-            await expo.sendPushNotificationsAsync(chunk);
-        }
-    } catch (error) {
-        console.error(error);
-    }
-});
+Deno.serve((_req) => new Response("Hello, world"));
