407e3a26b729cd0b3fe36a4d233bbf386e334cb2
katayama800 <tattu.0310@gmail.com>
2024-10-13 13:28:16 +0000

    Add push notification feature

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
-Deno.cron("Log a message", "* * * * *", () => {
-    console.log("This will print once a minute.");
+Deno.cron("Push notification", "* * * * *", async () => {
+    console.log("This will push a message to your device every minute.");
+    const pushMessage = {
+        to: "ExponentPushToken[yueW4qNgnfYmJ8r7eiO0rA]",
+        title: "hello",
+        body: "world",
+    };
+
+    try {
+        const response = await fetch("https://exp.host/--/api/v2/push/send", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(pushMessage),
+        });
+
+        if (!response.ok) {
+            console.error(
+                "Failed to send push notification:",
+                response.statusText,
+            );
+        } else {
+            console.log("Push notification sent successfully!");
+        }
+    } catch (error) {
+        console.error("Error sending push notification:", error);
+    }
 });
