bc82bedd32498381b90704634ca67102ac4e0223
katayama800 <tattu.0310@gmail.com>
2024-10-27 14:11:45 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,53 @@
-// import { Expo } from "expo-server-sdk";
+import { Expo } from "expo-server-sdk";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+    addDoc,
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    getFirestore,
+    query,
+    setDoc,
+    where,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { tripId, userId } from "./constant.ts";
+import type { Item } from "../types/item.ts";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdcXMgdlh4eJK8D4_KiVl1RHjCApJZ8VE",
+    authDomain: "lost-and-found-3f83f.firebaseapp.com",
+    projectId: "lost-and-found-3f83f",
+    storageBucket: "lost-and-found-3f83f.appspot.com",
+    messagingSenderId: "844445487537",
+    appId: "1:844445487537:web:a8c833490200eb085e2d90",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+const fetchPushToken = async (): Promise<string> => {
+    const pushTokens = await getDoc(
+        doc(db, "users", userId),
+    );
+
+    return pushTokens.data().pushToken as string;
+};
+
+const fetchItems = async (): Promise<Item[]> => {
+    const items = await getDocs(
+        query(
+            collection(db, "users", userId, "trips", tripId, "items"),
+        ),
+    );
+
+    console.log(items.docs.map((item) => item.data()));
+
+    return items.docs.map((item) => item.data()) as Item[];
+};
+
+// fetchPushToken();
+fetchItems();
 
 // const expo = new Expo();
 // Deno.cron("Push notification", "* * * * *", async () => {
@@ -24,4 +73,4 @@
 //     }
 // });
 
-Deno.serve((_req) => new Response("Hello, world!!!"));
+// Deno.serve((_req) => new Response("Hello, world!!!"));
---
+++ types/item.ts
@@ -0,0 +1,8 @@
+export type Item = {
+    isNotifyEnabled: true;
+    lastConfirmedAt: string | null;
+    lastNotifiedAt: string | null;
+    name: string;
+    reminderInterval: number;
+    status: string;
+};
